7b1533deab4c70e6a78b99758c26a87022d2da4a
Siddhartha <siddpurwar@gmail.com>
2021-11-16 00:32:00 +0530

    Revert "Reset password page and some refactoring"
    This reverts commit a0ca1f1181ae5024c1f140d99044ee58b0bbab0c.

--- flaskFile/forms.py
+++ flaskFile/forms.py
@@ -46,22 +46,20 @@ class Ride(FlaskForm):
 
 
 class UpdateAccountForm(FlaskForm):
+    #The SubmitField class represents an <input> element with a type="submit" attribute.
+    # The first argument to the Field constructors is the label that will be used when rendering the form to HTML.
+    #These are the attributes of RegestrationForm class
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Update')
 
     def validate_username(self, username):
-        if username.data != current_user.username:
-            user = User.query.filter_by(username=username.data).first()
-            if user:
-                raise ValidationError('That username is taken. Please choose a different one.')
-        else:
-            raise ValidationError('New Username cannot be same as curent Username')
+        
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
-        if email.data != current_user.email:
-            user = User.query.filter_by(email=email.data).first()
-            if user:
-                raise ValidationError('That email is taken. Please choose a different one.')
-        else:
-            raise ValidationError('New Email cannot be same as curent Email')
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
