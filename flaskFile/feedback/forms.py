3bc55ac05b0992885d19175084b7bc9d42185cb3
Siddhartha <siddpurwar@gmail.com>
2021-12-13 18:27:28 +0530

    Closes #4
    Feedback form was expecting a date value, but feedback model has default datetime.utc()
    
    Files:
        modified:   flaskFile/feedback/forms.py
        modified:   flaskFile/feedback/routes.py
        modified:   flaskFile/general/routes.py
        modified:   flaskFile/templates/feedback/all-feedbacks.html
        modified:   flaskFile/templates/feedback/my-feedback.html

--- flaskFile/feedback/forms.py
+++ flaskFile/feedback/forms.py
@@ -5,5 +5,4 @@ from wtforms.validators import Length, DataRequired
 class MyFeedback(FlaskForm):
     title = StringField('Title', validators=[Length(min=5, max=50), DataRequired()])
     content = TextAreaField('Feedback', validators=[Length(min=10, max=500)])
-    date = DateField('Feed back date', format='%m/%d/%Y', validators=[DataRequired()])
     submit = SubmitField('Send Feedback')
--- flaskFile/feedback/routes.py
+++ flaskFile/feedback/routes.py
@@ -1,33 +1,34 @@
-from flask import render_template, url_for, redirect, Blueprint
+from flask import render_template, url_for, redirect, Blueprint, request, flash
 from flaskFile.models import UserReviews
-from flask_login import login_required
+from flask_login import login_required, current_user
 from flaskFile.feedback.forms import MyFeedback
+from flaskFile import db
+from datetime import datetime
 
 feedback = Blueprint('feedback', __name__)
 
 @feedback.route('/my-feedback',  methods=['GET', 'POST'])
 @login_required
 def myFeedback():
-    return redirect(url_for('general.errorHandler'))
-    # form = MyFeedback()
-    # userFeedback = UserReviews.query.filter_by(author = current_user).first()
-    # if form.validate_on_submit():
-    #     if userFeedback != None:
-    #         userFeedback.title = form.title.data
-    #         userFeedback.content = form.title.data
-    #         userFeedback.dateOfReview = form.date.data
-    #         db.session.commit()
-    #         flash("Thank you for the Feedback!", "info")
-    #     else:      
-    #         userReview = UserReviews(title=form.title.data, content=form.content.data, author=current_user)
-    #         db.session.add(userReview)
-    #         db.session.commit()
-    #         flash("Thank you for the Feedback!", "info")
-    #     return redirect(url_for('feedback.allFeedback'))
-    # elif request.method == 'GET' and userFeedback:
-    #     form.title.data = userFeedback.title
-    #     form.content.data = userFeedback.content
-    # return render_template('feedback/my-feedback.html', title="My Feedback", form=form)
+    form = MyFeedback()
+    userFeedback = UserReviews.query.filter_by(author = current_user).first()
+    if form.validate_on_submit():
+        if userFeedback != None:
+            userFeedback.title = form.title.data
+            userFeedback.content = form.content.data
+            userFeedback.dateOfReview = datetime.utcnow()
+            db.session.commit()
+            flash("Thank you for the Feedback!", "info")
+        else:      
+            userReview = UserReviews(title=form.title.data, content=form.content.data, author=current_user)
+            db.session.add(userReview)
+            db.session.commit()
+            flash("Thank you for the Feedback!", "info")
+        return redirect(url_for('feedback.allFeedback'))
+    elif request.method == 'GET' and userFeedback:
+        form.title.data = userFeedback.title
+        form.content.data = userFeedback.content
+    return render_template('feedback/my-feedback.html', title="My Feedback", form=form)
 
 @feedback.route('/all-feedback')
 def allFeedback():
